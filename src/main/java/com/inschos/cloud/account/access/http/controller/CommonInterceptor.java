07696cae26b59cb02f08848a6ee766a9975557a7
meiming <meiming@bigwei.com>
2018-07-04 14:32:19 +0800

    origin

--- src/main/java/com/inschos/cloud/account/access/http/controller/CommonInterceptor.java
+++ src/main/java/com/inschos/cloud/account/access/http/controller/CommonInterceptor.java
@@ -40,7 +40,7 @@ public class CommonInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://122.14.202.146:9111");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
